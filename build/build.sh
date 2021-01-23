0c1dec664b6392cac426131b3bf36d3f74280207
linclaus <linclaus@outlook.com>
2021-01-23 12:55:55 +0800

    helm

---
+++ build/build.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+set -x
+
+CI_COMMIT_TAG=$(git describe --always --tags)
+
+docker build -t linclaus/grafana-operator:latest -f build/package/operator.Dockerfile .
+docker build -t linclaus/grafana-convert:latest -f build/package/converter.Dockerfile .
