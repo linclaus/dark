f36efbe984cfd3d9c30788299db6e7f7ab784a19
linclaus <linclaus@outlook.com>
2021-01-23 18:47:09 +0800

    converter

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,4 @@ set -x
 CI_COMMIT_TAG=$(git describe --always --tags)
 
 docker build -t linclaus/grafana-operator:latest -f build/package/operator.Dockerfile .
-docker build -t linclaus/grafana-convert:latest -f build/package/converter.Dockerfile .
+docker build -t linclaus/grafana-converter:latest -f build/package/converter.Dockerfile .
